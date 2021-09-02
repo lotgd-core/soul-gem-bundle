d538ff1342a9a83befa384c1bed6c68d4d78c538
IDMarinas <35842929+idmarinas@users.noreply.github.com>
2021-09-02 18:29:46 +0200

    Change translation domain

--- src/LotgdSoulGemBundle.php
+++ src/LotgdSoulGemBundle.php
@@ -21,7 +21,7 @@ final class LotgdSoulGemBundle extends Bundle implements LotgdBundleInterface
 {
     use LotgdBundleTrait;
 
-    public const TRANSLATION_DOMAIN = 'lodge_soul_gem';
+    public const TRANSLATION_DOMAIN = 'lotgd_soul_gem';
 
     /**
      * {@inheritDoc}
